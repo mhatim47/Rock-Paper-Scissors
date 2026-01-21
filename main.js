6ad6096a1a1298c2c2f12825fa3cc93f31e2d579
mhatim47 <mohamedhatime47@gmail.com>
2026-01-21 21:05:12 +0200

    i think this is a good logic , right?

--- main.js
+++ main.js
@@ -11,35 +11,39 @@ sicssor - rock -> lose
 */
 
 
-let btn = document.getElementById("btn");
-let pchoice = document.getElementsByName('p-choics');
-let compchoice = document.getElementById("c-choice");
-const form = document.querySelector("form");
+const computerChoice = document.getElementById("computer-choice");
+const playerChoice = document.getElementById("player-choice");
+const result = document.getElementById("result");
+const choices = ['paper', 'rock', 'scissors'];
 
-btn.addEventListener('click', (e) => {
-    const data = new FormData(form);
-    let player = getPlayerChoice(data)
-    let [winner, computerChoice] = game(player);
-    compchoice.innerHTML = `${computerChoice}, winner is : ${winner}`;
-    console.log(player);
-    e.preventDefault()
-    
-})
+const btns = document.getElementsByTagName('button');
+Array.from(btns).forEach(element => {
+    element.addEventListener('click', (e) => {
+        let player = e.target.id.toLowerCase()
+
+        let [winner, computer] = game(player);
+        computerChoice.innerText = computer;
+        playerChoice.innerText = player;
+        result.innerText = winner;
+    })   
+});
+
+
+
 
 
 const whoWin = (player, computer) => {
     if (player === 'paper') {
-    return computer === 'rock' ? 'player': computer === 'scissor' ? 'computer': 'draw';
+    return computer === 'rock' ? 'player': computer === 'scissors' ? 'computer': 'draw';
 } else if (player === 'rock') {
-    return computer === 'scissor' ? 'player': computer === 'paper' ? 'computer': 'draw';
-} else if (player === 'scissor') {
+    return computer === 'scissors' ? 'player': computer === 'paper' ? 'computer': 'draw';
+} else if (player === 'scissors') {
     return computer === 'paper' ? 'player': computer === 'rock' ? 'computer': 'draw';
 } else {
     return 'error'
 }
 }
 const game = (playerChoice) => {
-    const choices = ['paper', 'rock', 'scissor'];
     let player = playerChoice;
     let computerChoice = choices[Math.floor(Math.random() *3)]
     let winer = whoWin(player, computerChoice);
@@ -47,8 +51,3 @@ const game = (playerChoice) => {
     return [winer, computerChoice]
 }
 
-const getPlayerChoice = (data) => {
-    for (const entry of data) {
-        return entry[1].toLowerCase();
-    }
-}
